dd1690c7161d088bec41c0c4e00176daacc1aa44
Daniil Gusev <dgusev@estee.com>
2023-04-01 20:47:43 +0300

    Add encoder block[WIP]

--- transformers/layers/basic_blocks.py
+++ transformers/layers/basic_blocks.py
@@ -13,7 +13,6 @@ class LinearProjection(nn.Module):
     """
 
     def __init__(self, input_dim, output_dim):
-
         super(LinearProjection, self).__init__()
         self.projector = nn.Linear(input_dim, output_dim)
         # TODO: Apply weights init
@@ -38,7 +37,6 @@ class FeedForwardNetwork(nn.Module):
     """
 
     def __init__(self, dims, ratio=4, p=0.1):
-
         super(FeedForwardNetwork, self).__init__()
 
         self._dims = dims
@@ -58,4 +56,56 @@ class FeedForwardNetwork(nn.Module):
 
     def forward(self, x):
         x = self.mapping(x)
-        return
+        return x
+
+
+class EncoderBlock(nn.Module):
+    """Transformer encoder block"""
+
+    def __init__(self, seq_length: int, num_heads: int, hidden_dim: int, mlp_dim: int, norm_layer: nn.LayerNorm):
+        super().__init__()
+        self.num_heads = num_heads
+
+        # Possitional embedding
+        self.pos_embedding = nn.Parameter(torch.empty(1, seq_length, hidden_dim).normal_(std=0.02))
+
+        # Attention block
+        self.ln_1 = norm_layer(hidden_dim)
+        self.self_attention = nn.MultiheadAttention(hidden_dim, num_heads, batch_first=True)
+
+        # MLP block
+        self.ln_2 = norm_layer(hidden_dim)
+        self.mlp = 0
+
+    def forward(self, input: torch.Tensor):
+        x = input + self.pos_embedding
+        x = self.ln_1(x)
+        x, _ = self.self_attention(x)
+        x = x + input
+        y = self.ln_2(x)
+        # y = self.mlp(y) # TODO: make normal mlp
+        return x + y  # add layer norm in the end?
+
+
+class VisionTransformer(nn.Module):
+    """Vision Transformer as per https://arxiv.org/abs/2010.11929."""
+
+    def __init__(
+        self,
+        image_size: int,
+        patch_size: int,
+        # num_layers: int,
+        # num_heads: int,
+        hidden_dim: int,
+        mlp_dim: int,
+        norm_layer: nn.LayerNorm,
+        num_classes: int = 10,
+    ):
+        super().__init__()
+
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.hidden_dim = hidden_dim
+        self.mlp_dim = mlp_dim
+        self.num_classes = num_classes
+        self.norm_layer = norm_layer
